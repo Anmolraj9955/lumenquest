2ce8e55f14d93fca043b53dd48966bc88a9eb72b
Anmolraj9955 <rajanmol588@gmail.com>
2025-01-04 00:24:38 +0530

    add initial

---
+++ taskmanager/tasks/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class Task(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    status = models.CharField(max_length=20, choices=[
+        ('Pending', 'Pending'),
+        ('In Progress', 'In Progress'),
+        ('Completed', 'Completed')
+    ])
+    due_date = models.DateField()
+
+    def __str__(self):
+        return self.title
